0f8a141b19e2a7b10c2feb2df5b400af291949a7
Kenny <kcyam@uci.edu>
2020-06-12 20:02:25 -0700

    Added KennyScene with interactable scripts

---
+++ SHMUP Game Jam 1/Assets/Scripts/Nodes/Node.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Node : MonoBehaviour
+{
+    public Transform cameraPosition;
+    public List<Node> reachableNodes = new List<Node>();
+    [HideInInspector] public Collider2D col;
+
+
+    void Start()
+    {
+        col = GetComponent<Collider2D>();
+    }
+
+}
