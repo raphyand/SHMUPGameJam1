1b87357b261db452a34192445467078c21748f75
Arely2810 <32009112+Arely2810@users.noreply.github.com>
2020-06-12 18:50:09 -0700

    cursor_stuff

---
+++ SHMUP Game Jam 1/Assets/Scripts/ItemJazz.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ItemJazz : MonoBehaviour
+{
+    //public AudioClip myJazz;
+    public AudioSource jazzSource;
+
+    void Start()
+    {
+        jazzSource = GetComponent<AudioSource>();
+    }
+
+    public void OnMouseEnter()
+    {
+        Debug.Log("Play_Audio");
+        jazzSource.Play();
+    }
+
+    public void OnMouseExit()
+    {
+        Debug.Log("STOP_Audio");
+        jazzSource.Stop();
+    }
+}
