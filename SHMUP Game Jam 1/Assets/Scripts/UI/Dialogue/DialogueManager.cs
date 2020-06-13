9b9828b07e72526060ceeb530b5ca725702672b5
raphyand <raphyand@gmail.com>
2020-06-12 17:16:14 -0700

    Added script DialogueManager

---
+++ SHMUP Game Jam 1/Assets/Scripts/UI/Dialogue/DialogueManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+     public Text nameText;
+     public Text dialogueText;
+
+     private Queue<string> sentences;
+    // Start is called before the first frame update
+    void Start()
+    {
+          sentences = new Queue<string>(); // Cache a new spot in memory 
+    }
+
+    
+}
