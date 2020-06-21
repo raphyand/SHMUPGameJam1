02a36db8dae1bd55e2579306329aa197d3c87cf4
Arely2810 <32009112+Arely2810@users.noreply.github.com>
2020-06-20 19:28:22 -0700

    MainMenu

---
+++ SHMUP Game Jam 1/Assets/Scripts/MainMenuConroller.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuConroller : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject optionsMenu;
+    //this button starts the game
+    public void playGame()
+    {
+        SceneManager.LoadScene("Bar");
+    }
+
+    //this button can either show you how to play or something else
+    public void options()
+    {
+        mainMenu.SetActive(false);
+        optionsMenu.SetActive(true);
+    }
+
+    //this button goes back to main menu
+    public void back()
+    {
+        mainMenu.SetActive(true);
+        optionsMenu.SetActive(false);
+    }
+
+    //this button exits the game
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+}
